b6d06e37c12abdace88840dbc14bd468210bec04
alitafreshi <alitafreshi123@yahoo.com>
2025-09-29 19:12:10 +0300

    add theme module to the app module

--- app/src/main/java/com/tafreshiali/musify/MainActivity.kt
+++ app/src/main/java/com/tafreshiali/musify/MainActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.tafreshiali.theme.MusifyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            MusifyTheme {
+            }
         }
     }
 }
